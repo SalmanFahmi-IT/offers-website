fff5d4e8f63b43116bd089fdd3ea40c999f4f682
Fahmi Salman <55559223+SalmanFahmi-IT@users.noreply.github.com>
2019-12-22 21:21:34 +0100

    feat: add redux and config store (#1)

---
+++ src/store/configureStore.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+export default function configureStore(initialState) {
+    const store = createStore(reducers, initialState, compose(
+        applyMiddleware(thunk),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    ));
+    return store;
+}
